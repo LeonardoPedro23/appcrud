38668fa2e6569066545fe20bfa1cfcd696682d24
LeonardoPedro23 <Lonrdo565@gmail.com>
2024-11-13 21:39:14 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+    <script>
+        function clearForm() 
+            document.getElementById('nome').value = '';
+            document.getElementById('telefone').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('senha').value = '';
+          document.getElementById('id').value = '';
+        
+    </script>
