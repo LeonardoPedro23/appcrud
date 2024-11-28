01c730a9d1c3b72283d01b968c7d996e80189fd7
isaskeee <Lonrdo565@gmail.com>
2024-11-28 19:43:06 -0300

    28-11

--- usuarios_cadastro.php
+++ usuarios_cadastro.php
@@ -1,14 +1,22 @@
-<?php
+<?php 
+include 'principal_controller.php'; 
+
+
+if (!isset($_SESSION['email'])) {
+    header("Location: index.php"); 
+    exit();
+}
+
+include 'header.php'; 
 include 'usuarios_controller.php';
-include 'header.php';
-//Pega todos os usuários para preencher os dados da tabela
+
+
 $users = getUsers();
 
-//Variável que guarda o ID do usuário que será editado
+
 $userToEdit = null;
 
-// Verifica se existe o parâmetro edit pelo método GET
-// e sé há um ID para edição de usuário
+
 if (isset($_GET['edit'])) {
     $userToEdit = getUser($_GET['edit']);
 }
@@ -31,55 +39,56 @@ if (isset($_GET['edit'])) {
     </script>
 </head>
 <body>
+    <div class="container mt-3">
     <h2>Cadastro de Usuários</h2>
     <form method="POST" action="">
         <input type="hidden" id="id" name="id" value="<?php echo $userToEdit['id'] ?? ''; ?>">
         
         <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" value="<?php echo $userToEdit['nome'] ?? ''; ?>" required><br><br>
+        <input class="form-control" type="text" id="nome" name="nome" value="<?php echo $userToEdit['nome'] ?? ''; ?>" required><br>
         
         <label for="telefone">Telefone:</label>
-        <input type="text" id="telefone" name="telefone" value="<?php echo $userToEdit['telefone'] ?? ''; ?>" required><br><br>
+        <input class="form-control" placeholder="(00) 0 0000-0000" type="text" id="telefone" name="telefone" value="<?php echo $userToEdit['telefone'] ?? ''; ?>" required><br>
         
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?php echo $userToEdit['email'] ?? ''; ?>" required><br><br>
+        <input class="form-control" placeholder="example@gmail.com" type="email" id="email" name="email" value="<?php echo $userToEdit['email'] ?? ''; ?>" required><br>
         
         <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
+        <input class="form-control" type="password" id="senha" name="senha" required><br><br>
         
-        <button type="submit" class="btn btn-success" name="save">Salvar</button>
-        <button type="submit" class="btn btn-warning" name="update">Atualizar</button>
-        <button type="button" class="btn btn-info" onclick="clearForm()">Novo</button>
+        <div class="btn-group" style="gap: 10px;">
+    <button type="submit" class="btn btn-success" name="save">Salvar</button>
+    <button type="submit" class="btn btn-secondary" name="update">Atualizar</button>
+    <button type="button" class="btn btn-warning" onclick="clearForm()">Novo</button>
+    </div>
     </form>
+    </div>
 
-    <h2>Usuários Cadastrados</h2>
     <div class="container mt-3">
-  <table class="table table-dark table-striped">
-        <tr>
+    <h2>Usuários Cadastrados</h2><br>
+    <table class="table table-hover" border="1">
+        <tr class="thead-dark">
             <th>ID</th>
             <th>Nome</th>
-            <th>Senha</th>
             <th>Telefone</th>
             <th>Email</th>
             <th>Ações</th>
         </tr>
-        <!--Faz um loop FOR no resultset de usuários e preenche a tabela-->
+      
         <?php foreach ($users as $user): ?>
-            <tr>
-                <td><?php echo $user['id']; ?></td>
-                <td><?php echo $user['nome']; ?></td>
-                <td><?php echo $user['senha']; ?></td>
-                <td><?php echo $user['telefone']; ?></td>
-                <td><?php echo $user['email']; ?></td>
-                
-                <td>
-                    <a href="?edit=<?php echo $user['id']; ?>" class="btn btn-danger">Editar</a>
-                    <a href="?delete=<?php echo $user['id']; ?>" class="btn btn-primary" onclick="return confirm('Tem certeza que deseja excluir?');">Excluir</a>
-                </td>
-            </tr>
+                <tr>
+                    <td><?php echo $user['id']; ?></td>
+                    <td><?php echo $user['nome']; ?></td>
+                    <td><?php echo $user['telefone']; ?></td>
+                    <td><?php echo $user['email']; ?></td>
+                    <td>
+                        <a href="?edit=<?php echo $user['id']; ?>" class="btn btn-secondary btn-sm">Editar</a>
+                        <a href="?delete=<?php echo $user['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm ('Tem certeza que deseja excluir este usuário?');">Excluir</a>
+                    </td>
+                </tr>
         <?php endforeach; ?>
     </table>
-        </div>
+    </div>
     <?php include 'footer.php'; ?>
 </body>
 </html>
